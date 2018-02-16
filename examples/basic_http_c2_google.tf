1e5ce438684931adc30e688e51a596d9b35a48a8
devnet <devnet@devnetbase>
2018-02-16 17:25:03 -0500

    Add Google Compute

---
+++ examples/basic_http_c2_google.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.10.0"
+}
+
+module "http_c2" {
+  source = "./modules/google/http-c2"
+  count = 1
+}
+
+module "http_rdir" {
+  source = "./modules/google/http-rdir"
+  count = 2
+  redirect_to = "${module.http_c2.ips}"
+  zones = ["Canada-1", "Brazil-2"]
+}
