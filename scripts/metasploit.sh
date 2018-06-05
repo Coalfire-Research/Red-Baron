8e5920f85772b160b223e2294a3c1266a12909ea
byt3bl33d3r <byt3bl33d3r@protonmail.com>
2018-06-05 15:52:22 -0600

    Updated metasploit install

--- scripts/metasploit.sh
+++ scripts/metasploit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get install -y libpq-dev libpcap0.8-dev
-gpg --keyserver hkp://keys.gnupg.net:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
+source /etc/profile.d/rvm.sh
 git clone https://github.com/rapid7/metasploit-framework
 cd metasploit-framework
 rvm install ruby-`(cat .ruby-version)`
