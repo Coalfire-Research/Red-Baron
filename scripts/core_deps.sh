2ce1249034c9d7350a141e37495096256fb02dee
byt3bl33d3r <byt3bl33d3r@protonmail.com>
2018-09-19 02:40:17 +0800

    Added mosh to core_deps.sh

--- scripts/core_deps.sh
+++ scripts/core_deps.sh
@@ -4,7 +4,7 @@
 # have absolutely zero clue what's going on, but just running the command again seems to fix it
 
 sudo apt-get update
-cmd="sudo apt-get install -y tmux git dirmngr debconf-utils curl wget build-essential vim gcc socat certbot"
+cmd="sudo apt-get install -y tmux git dirmngr debconf-utils curl wget build-essential vim gcc socat certbot mosh"
 until eval $cmd
 do
     sleep 10
